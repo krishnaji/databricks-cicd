4dabdc4d3d2bdb18bfe9ba69edb3ac8652811d2e
Shrikrishna Khose <krishna.khose@gmail.com>
2018-12-02 00:57:42 -0500

    New Change

--- etl/common/Azure Blob Storage Import Example Notebook.py
+++ etl/common/Azure Blob Storage Import Example Notebook.py
@@ -14,9 +14,11 @@
 # MAGIC To get started, we need to set the location and type of the file.
 
 # COMMAND ----------
+
 import os
 storage_account_name = "clinicaltrialsdata"
 storage_account_access_key = os.environ['STORAGE_KEY']
+print("new change")
 
 # COMMAND ----------
 
